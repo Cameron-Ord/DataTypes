dc4bac6b86cc3b11edc6ec0cbc39ce717da0792e
cameron <hammerbro_97@hotmail.com>
2023-03-16 18:57:22 -0600

    useless numbers

--- js/app.js
+++ js/app.js
@@ -6,3 +6,10 @@ if(age == `${age}` && user_name == `${user_name}`){
 console.log(`Your name is ${user_name} and you are ${age} years old.`);
 
 }
+
+let max_volume = 5000;
+let current_volume = 3000;
+
+
+console.log(max_volume / current_volume);
+console.log(max_volume / current_volume * 100);
